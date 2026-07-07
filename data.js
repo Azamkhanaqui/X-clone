6ebff9128fbf59b5644ca7515bbb617d6dfe9c6f
Azam Khan <110544985+Azamkhanaqui@users.noreply.github.com>
2026-07-08 01:21:50 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,79 @@
+export const tweetsData = [   
+    {
+        handle: `@Saitama ✅`,
+        profilePic: `images/sai.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Beat today's final boss.
+                        Still missed the supermarket sale.😐🥬`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+        isVerified: false
+    },    
+    {
+        handle: `@Goku `,
+        profilePic: `images/goku.png`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `Anyone strong wants to spar?👊😁`,
+        isVerified: true,
+        replies: [
+                  {
+                handle: `@GojoSatoru`,
+                profilePic: `images/gojo.png`,
+                tweetText: `Only if you're buying lunch after.😎☕`,
+                      likes: 500,
+                      retweets: 394,
+                      replies: 105,
+                      uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs0',
+                      isVerified: true
+
+            },
+                  {
+                handle: `@SungJinwoo `,
+                profilePic: `images/sung.jpg`,
+                tweetText: `I'm free after this dungeon⚔️`,
+                      likes: 65,
+                      retweets: 94,
+                      replies: 590,
+                      uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs8',
+                      isVerified: true
+
+                  },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@Luffy`,
+        profilePic: `images/luffy.jpg`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `WHO ATE MY MEAT?? 🍖😡`,
+        replies: [
+            {
+                handle: `@Denji`,
+                profilePic: `images/denji.png`,
+                tweetText: `No idea... 👀`,
+                likes: 2,
+                retweets: 1,
+                replies: 15
+
+            },
+            {
+                handle: `@AnayaForger`,
+                profilePic: `images/anya.png`,
+                tweetText: `...Sorry. 🥔🍖️`,
+                likes: 5,
+                retweets: 3,
+                replies: 5
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
